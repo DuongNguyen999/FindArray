606bfeec8d7bbd592efab7b4e664c32b30a585f9
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-04-09 21:50:24 +0900

    Add du an
    Add du an

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+public static void Main(string[] args)
+{
+    string[] mystudents = {"Mai","An","Hao","Trung","Dung","Long"};
+Console.WriteLine("Nhap 1 cai ten tu ban phim");
+string name = Console.ReadLine();
+
+bool isHas=false;
+for (int i = 0; i < mystudents.Length; i++)
+{
+    if (mystudents[i].Equals(name))
+    {
+        Console.WriteLine("Co sinh vien nay");
+        isHas = true;
+        break;
+    }
+}
+    if(!isHas)
+    {
+    Console.WriteLine("Khong co");
+    }
+}
+}
+
